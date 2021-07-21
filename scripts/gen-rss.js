eefd4802694e30397197b3f5f6bdd29be3a00b29
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-21 15:10:29 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'xiaowuc2',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    site_url: 'https://xiaowuc2.vercel.app',
+    feed_url: 'https://xiaowuc2.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
