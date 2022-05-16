8b604dcbe2599149b01dbc3e8d8d5945488261b8
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2022-05-16 21:13:25 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '80rem' }}>
+    <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © xiaowuc2
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
